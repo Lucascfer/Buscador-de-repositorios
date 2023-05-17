7bf1f98995527ac41798a2b2c4bd7d9baa91f37c
Lucas Carvalho <lucascf000@gmail.com>
2023-05-17 16:29:24 -0300

    font-family added

--- app.vue
+++ app.vue
@@ -5,7 +5,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 * {
+  font-family: 'Inter', sans-serif;
   color: #000;
   text-decoration: none;
   margin: 0;
